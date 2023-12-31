8a8e386d41e8711e07be81e96754a2b41aa4e76d
NagendraSethi <pro.nagendrasethi@gmail.com>
2023-12-31 18:21:41 +0530

    data ingestion

--- main.py
+++ main.py
@@ -4,6 +4,11 @@ from Insurance.utils import get_collection_as_dataframe
 import sys, os
 from Insurance.entity.config_entity import DataIngestionConfig
 from Insurance.entity import config_entity
+from Insurance.components.data_ingestion import DataIngestion
+# from Insurance.components.data_validation import DataValidation
+
+# from Insurance.components.data_transformation import DataTransformation
+
 #def test_logger_and_expection():
    # try:
        # logging.info("Starting the test_logger_and_exception")
@@ -24,13 +29,13 @@ if __name__=="__main__":
       #data ingestion
        data_ingestion_config  = config_entity.DataIngestionConfig(training_pipeline_config=training_pipeline_config)
        print(data_ingestion_config.to_dict())
-      #  data_ingestion = DataIngestion(data_ingestion_config=data_ingestion_config)
-      #  data_ingestion_artifact = data_ingestion.initiate_data_ingestion()
+       data_ingestion = DataIngestion(data_ingestion_config=data_ingestion_config)
+       data_ingestion_artifact = data_ingestion.initiate_data_ingestion()
        
        # Data Validation
        data_validation_config = config_entity.DataValidationConfig(training_pipeline_config=training_pipeline_config)
       #  data_validation = DataValidation(data_validation_config=data_validation_config,
-      #                    data_ingestion_artifact=data_ingestion_artifact)
+                        #  data_ingestion_artifact=data_ingestion_artifact)
         
       #  data_validation_artifact = data_validation.initiate_data_validation()
 
@@ -38,7 +43,7 @@ if __name__=="__main__":
 
        data_transformation_config = config_entity.DataTransformationConfig(training_pipeline_config=training_pipeline_config)
       #  data_transformation = DataTransformation(data_transformation_config=data_transformation_config, 
-      #  data_ingestion_artifact=data_ingestion_artifact)
+       data_ingestion_artifact=data_ingestion_artifact
       #  data_transformation_artifact = data_transformation.initiate_data_transformation()
 
       
